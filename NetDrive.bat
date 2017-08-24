6dd6e292e78630063999d8a92dfd99fcd28ebb32
Emin Aliyev <user084@gmail.com>
2017-08-25 00:43:44 +0400

    Batch

--- NetDrive.bat
+++
@@ -1,8 +0,0 @@
-@Echo
-if exist L: GOTO STEP1
-if not exist L: net use \\10.10.1.50\VasBackup /d /yes | net use L: /d /yes | net use L: \\10.10.1.50\VasBackup /User:azerfon\vasuser 123456789F!
-
-
-
-:STEP1
-echo exit
